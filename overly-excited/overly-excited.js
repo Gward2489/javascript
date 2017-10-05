787d203849ca9c13a95fdf50240a21300805fd70
Gward2489 <gww8804@gmail.com>
2017-10-05 15:59:05 -0500

    added working excited functions loop

---
+++ overly-excited/overly-excited.js
@@ -0,0 +1,23 @@
+let sentence = ["The", "walrus", "danced", "through", "the", "trees", "in", "the", "light", "of", "the", "moon"]
+
+let betterSentence = ["The", "wizards", "rapped", "all", "evening", "long", "grammys", "they", "won", "cheer", "they", "had", "stoke", "they", "spread"]
+
+function addExcitement (options) {
+    let output = ""
+    let j = 0
+    let bang = options.punctuation
+
+    for (let i = 0; i < options.array.length; i++) {
+        output += options.array[i]
+        j++
+        if (j % 3 === 0) {
+            output += bang
+            bang += options.punctuation
+            }
+            output += " "
+            console.log(output)
+        } 
+          
+    }
+
+addExcitement({"array": betterSentence, "punctuation": "?"}) 
