4d3f2a269974d389cda68b61aa8c9525bd0e7d6e
Gward2489 <gww8804@gmail.com>
2017-10-25 10:28:09 -0500

    pushing fixed version again

--- cash-to-coins/cashToCoins.js
+++ cash-to-coins/cashToCoins.js
@@ -1,5 +1,3 @@
-//Does not work right
-
 //set dollar amount
 let dollarAmount = 450.34
 
@@ -46,7 +44,7 @@ for (property in piggyBank) {
             piggyBank.nickels = j
         } else if (property === "pennies") {
             let j = 0
-            while (dollarAmount > .01) {
+            while (dollarAmount >= 0) {
                 dollarAmount -= .01
                 j++
             }
@@ -55,114 +53,3 @@ for (property in piggyBank) {
     }
 
 console.log(piggyBank)
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-function calc (cash) {
-    let j = 0
-    let i = 0
-    let y = 0
-    let x = 0
-
-    for (property in piggyBank)
-        if (property === "quarters") {
-            j = cash / .25
-            i = cash - j
-            y = (j / 2)
-            piggyBank.quarters = j.toFixed(0) - y.toFixed(0)
-        } else if (property === "dimes") {
-            j = piggyBank.quarters * .25
-            i = cash - j
-            y = i / .10
-            x = (y / 2)
-            piggyBank.dimes = y.toFixed(0) - x.toFixed(0) 
-        } else if (property === "nickels") {
-            j = piggyBank.dimes * .10
-            i = cash - j
-            y = i / .05
-            x = (y / 2)
-            piggyBank.nickels = y.toFixed(0) - x.toFixed(0) 
-        } else if (property === "pennies") {
-            j = piggyBank.nickels * .05
-            i = cash - j
-            y = i / .01
-            x = (y / 2)
-            piggyBank.pennies = y.toFixed(0) - x.toFixed(0)
-        } 
-        
-}
-
-calc(dollarAmount)
-
-console.log(piggyBank)
-
-*/
-
-/* Fail #2
-function calc (cash) {
-    let j = 0
-
-    for (property in piggyBank) {
-        if (property === "quarters") {
-            j = cash / .25
-            Math.floor(j)
-            piggyBank.quarters = j
-        } else if (property === "dimes") {
-            j = cash / .10
-            piggyBank.dimes = j.toFixed(0)
-        } else if (property === "nickels") {
-            j = cash / .05
-            piggyBank.nickels = j.toFixed(0)
-        } else if (property === "pennies") {
-            j = cash / .01
-            piggyBank.pennies = j.toFixed(0)
-        }
-    
-        
-    }
-}
-
-calc(dollarAmount)
-
-console.log(piggyBank)
-*/
-
-
-/* Does not work because const does not lock the 206.66 value, just the variable type 
- of dollarAmount which is a number */ 
-
-/* 
-const dollarAmount = 206.66
-
-piggyBank = {
-    "quarters": 0,
-    "dimes": 0,
-    "nickels": 0,
-    "pennies": 0
-}
-
-let floatingTotal = 0
-
-piggyBank.quarters = (dollarAmount / .25).toFixed(0)
-floatingTotal = dollarAmount - (piggyBank.quarters * .25)
-piggyBank.dimes = (floatingTotal / .10).toFixed(0)
-floatingTotal = dollarAmount - ((piggyBank.quarters * .25) + (piggyBank.dimes * .10))
-piggyBank.nickels = (floatingTotal / .05).toFixed(0)
-floatingTotal = dollarAmount - ((piggyBank.nickels * .5) + (piggyBank.dimes * .10) + (piggyBank.quarters * .25))
-piggyBank.pennies = (floatingTotal / .01).toFixed(0)
-
-
-console.log(piggyBank)
-
-*/
-
