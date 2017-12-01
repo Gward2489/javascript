63d68afdca9b776ca522fa77b7f619f72a425ed4
Gward2489 <gww8804@gmail.com>
2017-12-01 15:53:26 -0600

    working crud operations for angular employees with working agular order by

--- angular-employees/app/EmployeeCtrl.js
+++ angular-employees/app/EmployeeCtrl.js
@@ -3,39 +3,67 @@ var app = angular.module("EmployeeMgmt", [])
 
 app.controller("EmployeeCtrl", function ($scope, $http) {
     
-    
+getEmployees = () => {
     $http
     .get("https://employees-dem.firebaseio.com/.json")
     .then(
         function(firebaseObjectOfObjects) {
-            
-            $scope.employees = firebaseObjectOfObjects.data.employees
-            
-        }
-    )
+
+            $scope.employees = Object.keys(firebaseObjectOfObjects.data.employees)
+            .map(key => {
+                firebaseObjectOfObjects.data.employees[key].id = key
+                return firebaseObjectOfObjects.data.employees[key]
+            })
+
+            console.log($scope.employees)
+
+        })
+}
     
+    $scope.murderEmployee = function(employee) {
+        console.log(employee)
+        $http
+        .delete(`https://employees-dem.firebaseio.com/employees/${employee.id}/.json`)
+        .then( function () {
+            getEmployees()
+        })
+    }
+
     $scope.fireEmployee = function(employee) {
         employee.employmentEnd = Date.now()
+
+        $http
+        .put(`https://employees-dem.firebaseio.com/employees/${employee.id}/.json`,
+        employee).then(function () {
+            getEmployees()
+        })
     }
 
     $scope.createEmployee = function (firstName, lastName) {
 
-        let newId = $scope.employees.length + 1
-
         let newEmployee = {
-            "id": newId,
+
             "firstName": firstName,
             "lastName": lastName,
-            "employementStart": Date.now(),
-            "employmentEnd": null
+            "employmentStart": Date.now(),
+            "employmentEnd": 0
         }
 
-        $scope.employees.push(newEmployee)
-        $scope.firstName = ""
-        $scope.lastName = ""
+        $http
+        .post(`https://employees-dem.firebaseio.com/employees/.json`,
+        newEmployee
+        ).then( () => {
+            $scope.firstName = ""
+
+            $scope.lastName = ""
+
+            getEmployees()
+        })
+        
     } 
 
-  
+getEmployees()
+
 })
 
 
